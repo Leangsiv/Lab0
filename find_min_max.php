1e133f76f7676d033221b21f008c1bda6f2ee9dc
Steven <steven@MacBooks-MacBook-Pro.local>
2019-05-21 11:05:07 +0700

    edit2

--- find_min_max.php
+++ find_min_max.php
@@ -1,4 +1,17 @@
 <?php
-	$array[10] = {1,2,3,4,5,6,7,8,9,10};
-	echo $array[1];
+	echo "<b style='color:red;'>Finding Max and Min value from an array without using the built-in function (max and min).</b>"."<br>";
+	$arr = array(1,2,3,4,5,6,7,8,9,10);
+	$max = $arr[0];
+	$min = $arr[0];
+	for ($i = 1 ; $i < 10 ; $i++){
+		if($max < $arr[$i]){
+			$max = $arr[$i];
+		}
+		if($min > $arr[$i]){
+			$min = $arr[$i];
+		}
+	}
+	echo "Max = ".$max."<br>";
+	echo "Min = ".$min."<br>";
+	
 ?>
